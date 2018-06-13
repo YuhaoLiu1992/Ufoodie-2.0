eae09e423894817faa8e78226cdd8918aa601b9b
Yuhao Liu <u5463450@anu.edu.au>
2018-06-13 12:45:36 +1000

    Add Side Menu

--- Ufoodie 2.0/ViewController.swift
+++ Ufoodie 2.0/ViewController.swift
@@ -13,11 +13,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var ufoodProgress: UIProgressView!
     @IBOutlet weak var ufoodWebview: WKWebView!
+    @IBOutlet weak var viewConstraint: NSLayoutConstraint!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var sideView: UIView!
+    @IBOutlet weak var sideViewConstraint: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        blurView.layer.cornerRadius = 15
+        
+        sideViewConstraint.constant = -175
+        
+        
         let url = URL(string: "https://ufoodie.com.au")
         let request = URLRequest(url: url!)
         ufoodWebview.load(request)
@@ -35,6 +44,48 @@ class ViewController: UIViewController {
         }
     }
 
+    @IBAction func panPerformed(_ sender: UIPanGestureRecognizer) {
+
+            if sender.state == .began || sender.state == .changed {
+                
+                let translation = sender.translation(in: self.view).x
+                
+                if translation > 0 { // Swipe Right
+                    if sideViewConstraint.constant < 20 {
+                        UIView.animate(withDuration: 0.2, animations: {
+                            self.sideViewConstraint.constant += translation
+                            self.view.layoutIfNeeded()
+                        })
+                                            }
+                } else {             // Swipe Left
+                    
+                    if sideViewConstraint.constant > -175 {
+                        UIView.animate(withDuration: 0.2, animations: {
+                            self.sideViewConstraint.constant += translation
+                            self.view.layoutIfNeeded()
+                        })
+                    }
+                    
+                }
+                
+            } else if sender.state == .ended {
+                
+                if sideViewConstraint.constant < -100 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.sideViewConstraint.constant = -175
+                        self.view.layoutIfNeeded()
+                    })
+                } else {
+                    if sideViewConstraint.constant > -175 {
+                        UIView.animate(withDuration: 0.2, animations: {
+                            self.sideViewConstraint.constant = 0
+                            self.view.layoutIfNeeded()
+                        })
+                    }
+                }
+            }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
